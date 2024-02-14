9e687a9125b478d435a3c8fe7b3ba553dcfad067
hashimoto takumi <takumih6013@gmail.com>
2024-02-14 14:18:37 +0900

    リモートリポジトリの変更

---
+++ src/app/Http/Controllers/AuthorController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+use App\Http\Requests\AuthorRequest;
+
+
+class AuthorController extends Controller
+{
+    public function index()
+    {
+        $authors = Author::all();
+        return view('index', ['authors' => $authors]);
+    }
+
+    public function find()
+    {
+        return view('find', ['input' => '']);
+    }
+    public function search(Request $request)
+    {
+        $item = Author::where('name', $request->input)->first();
+        $param = [
+            'input' => $request->input,
+            'item' => $item
+        ];
+        return view('find', $param);
+    }
+
+
+
+    public function add()
+    {
+        return view('add');
+    }
+    public function create(AuthorRequest $request)
+    {
+        $form = $request->all();
+        Author::create($form);
+        return redirect('/');
+    }
+    public function edit(Request $request)
+    {
+        $author = Author::find($request->id);
+        return view('edit', ['form' => $author]);
+    }
+    public function update(AuthorRequest $request)
+    {
+        $form = $request->all();
+        unset($form['_token']);
+        Author::find($request->id)->update($form);
+        return redirect('/');
+    }
+    public function delete(Request $request)
+    {
+        $author = Author::find($request->id);
+        return view('delete', ['author' => $author]);
+    }
+    public function remove(Request $request)
+    {
+        Author::find($request->id)->delete();
+        return redirect('/');
+    }
+    public function bind(Author $author)
+    {
+        $data = [
+            'item' => $author,
+        ];
+        return view('author.binds', $data);
+    }
+    
+    public function verror()
+    {
+    return view('verror');
+    }
+}
+
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [AuthorController::class, 'index']);
+Route::get('/add', [AuthorController::class, 'add']);
+Route::post('/add', [AuthorController::class, 'create']);
+Route::get('/edit', [AuthorController::class, 'edit']);
+Route::post('/edit', [AuthorController::class, 'update']);
+Route::get('/delete', [AuthorController::class, 'delete']);
+Route::post('/delete', [AuthorController::class, 'remove']);
+Route::get('/find', [AuthorController::class, 'find']);
+Route::post('/find', [AuthorController::class, 'search']);
+Route::get('/author/{author}', [AuthorController::class, 'bind']);
+Route::get('/verror', [AuthorController::class, 'verror']);
